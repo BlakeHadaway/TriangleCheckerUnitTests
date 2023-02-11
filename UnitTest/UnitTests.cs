2e1050bbfed9895c570e027742672337e8c5d0ea
Blake James Hadaway <Blake.j.hadaway@gmail.com>
2023-02-11 08:32:24 -0500

    Added unit test for Isosceles triangle with various inputs

--- UnitTest/UnitTests.cs
+++ UnitTest/UnitTests.cs
@@ -29,5 +29,56 @@ namespace UnitTest
             Assert.AreEqual(expected, actual);
 
         }
+
+        [Test]
+        public void Triangle_Input_3_3_6_VerifyIsosceles()
+        {
+            //Arrange
+            int side1 = 4;
+            int side2 = 4;
+            int side3 = 6;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Triangle_Input_9_4_9_VerifyIsosceles()
+        {
+            //Arrange
+            int side1 = 9;
+            int side2 = 4;
+            int side3 = 9;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Triangle_Input_27_32_32_VerifyIsosceles()
+        {
+            //Arrange
+            int side1 = 27;
+            int side2 = 32;
+            int side3 = 32;
+
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
